2046f4069ea207f22820e88b1d16197fef0ad20d
Happony <sthursbyg@gmail.com>
2016-10-11 18:56:29 -0400

    I did comments ( *) = ( *)

--- FRC3620_2016_GitPractice/src/org/usfirst/frc3620/FRC3620_2016_GitPractice/OI.java
+++ FRC3620_2016_GitPractice/src/org/usfirst/frc3620/FRC3620_2016_GitPractice/OI.java
@@ -8,7 +8,7 @@
 // update. Deleting the comments indicating the section will prevent
 // it from being updated in the future.
 
-
+//Iwas 
 package org.usfirst.frc3620.FRC3620_2016_GitPractice;
 
 import org.usfirst.frc3620.FRC3620_2016_GitPractice.commands.*;
